7e4ed89ea79ee0d15ecd947d038ba7c5908cc871
MohsinBabaSangram <bamohsin@pagoanalytics.com>
2023-10-21 21:55:20 +0530

    Initial Commit

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import AppNavigator from './routes/AppNavigator';
+console.reportErrorsAsExceptions = false;
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+     <AppNavigator/>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
+    width:"100%",
   },
 });
+
